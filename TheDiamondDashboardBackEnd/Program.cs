3ae456f91c34c2cea60f32e0b10f975d4f371666
Kian Bruyenberg <kbruyenberg@gmail.com>
2023-11-16 15:09:22 +0100

    Policy toegevoegd om te zorgen dat de frontend de api kan benaderen.

--- TheDiamondDashboardBackEnd/Program.cs
+++ TheDiamondDashboardBackEnd/Program.cs
@@ -5,6 +5,16 @@ using TheDiamondDashboardBackEnd.Services.ManagerService;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactPolicy", builder =>
+    {
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("ReactPolicy");
 
 app.UseHttpsRedirection();
 
