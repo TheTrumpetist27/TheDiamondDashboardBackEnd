ca52ac246266443f2ba29799df3c715e804df4c7
Kian Bruyenberg <kbruyenberg@gmail.com>
2023-11-29 13:38:16 +0100

    Update HttpHelper.cs

--- TheDiamondDashboardBackEndTests/Helper/HttpHelper.cs
+++ TheDiamondDashboardBackEndTests/Helper/HttpHelper.cs
@@ -7,6 +7,7 @@
             public readonly static string Manager = "api/manager";
             public readonly static string ManagerId1 = "api/manager/1";
             public readonly static string ManagerId2 = "api/manager/2";
+            public readonly static string ManagerId3 = "api/manager/3";
         }
     }
 }
