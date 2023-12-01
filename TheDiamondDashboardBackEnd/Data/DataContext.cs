db9d3aeebc92bac7aa407dab8af934bab943b1d5
Kian Bruyenberg <kbruyenberg@gmail.com>
2023-12-01 13:44:57 +0100

    cleanup some code

--- TheDiamondDashboardBackEnd/Data/DataContext.cs
+++ TheDiamondDashboardBackEnd/Data/DataContext.cs
@@ -4,25 +4,10 @@ namespace TheDiamondDashboardBackEnd.Data
 {
     public class DataContext : DbContext
     {
-        //static readonly string connectionString = "server=localhost;port=3306;database=thediamonddashboarddb;user=diamond;password=dashboard";
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
             
         }
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    base.OnConfiguring(optionsBuilder);
-
-        //    IConfiguration configuration = new ConfigurationBuilder()
-        //    .SetBasePath(Directory.GetCurrentDirectory())
-        //    .AddJsonFile("appsettings.json")
-        //    .Build();
-
-        //    string? connectionString = configuration.GetConnectionString("DefaultConnection");
-
-        //    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-        //}
 
         public DbSet<Manager> manager { get; set; }
     }
